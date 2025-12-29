ab6cb64039fd1f577095b097cb1e08fbb98482c8
SeanYoung1 <younggseann16@gmail.com>
2025-12-29 01:29:51 -0800

    Refactor SQL queries and add device management functions
    Updated SQL queries to remove semicolons and added new device management functions.

--- database.py
+++ database.py
@@ -13,6 +13,7 @@ DB_NAME = "health_rivals.db"
 def get_connection():
     """Establish a connection to the SQLite database."""
     conn = sqlite3.connect(DB_NAME)
+    # This allows us to access the columns by name
     conn.row_factory = sqlite3.Row
     return conn
 
@@ -41,7 +42,7 @@ def retrieve_person(User_ID):
     cursor.execute("""
          SELECT User_ID, Email, Credit, FName, Middle_Initial, Lname, Height, Birthday
          FROM Person
-         WHERE User_ID = ?;
+         WHERE User_ID = ?
     """, (User_ID))
 
     # Fetch one row from the result set
@@ -91,7 +92,7 @@ def add_activities_type(Activities_ID, ATypes):
     
     cursor.execute("""
           INSERT INTO Activities_Type (Activities_ID, ATypes)
-          VALUES (?, ?);
+          VALUES (?, ?)
     """, (Activities_ID, ATypes))
     
     conn.commit()
@@ -456,6 +457,10 @@ def add_challenge(Challenge_ID, Daily_Challenge, Challenge_Complete, DailyRivals
         INSERT INTO Challenges (Challenge_ID, Daily_Challenge, Challenge_Complete, DailyRivals_Credit, Special_Challenge)
         VALUES (?, ?, ?, ?, ?)
     """, (Challenge_ID, Daily_Challenge, Challenge_Complete, DailyRivals_Credit, Special_Challenge))
+
+    conn.commit()
+    conn.close()
+
     
 def retrieve_challenge(Challenge_ID):
     conn = get_connection()
@@ -479,7 +484,7 @@ def update_challenges_completion(Challenge_ID):
     cursor.execute("""
         UPDATE Challenges
         SET Challenge_Complete = TRUE
-        WHERE Challenge_ID = ?;
+        WHERE Challenge_ID = ?
     """, (Challenge_ID))
 
     conn.commit()
@@ -508,3 +513,380 @@ def apply_challenge_credit():
     conn.commit()
     conn.close()
     
+
+# device management
+
+def add_device(Device_ID, Phone, Smart_watch, Implanted_chip, PU_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        INSERT INTO Device (Device_ID, Phone, Smart_watch, Implanted_chip, PU_ID)
+        VALUES (?, ?, ?, ?, ?)
+    """, (Device_ID, Phone, Smart_watch, Implanted_chip, PU_ID))
+    
+    conn.commit()
+    conn.close()
+
+def retrieve_device(Device_ID, Phone, Smart_watch, Implanted_chip, PU_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+           SELECT Device_ID, Phone, Smart_watch, Implanted_chip, PU_ID
+           FROM Device
+           WHERE PU_ID = ?
+    """, (Device_ID, Phone, Smart_watch, Implanted_chip, PU_ID))
+
+    device = cursor.fetchnone()
+    conn.close()
+    return dict(device) if device else None
+
+def update_devices(PU_ID, Phone, Smart_watch, Implanted_chip):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        UPDATE Device
+        SET Phone = FALSE,
+        Smart_watch = TRUE,
+        Implanted_chip = TRUE
+        WHERE PU_ID = ?
+    """, (PU_ID))
+
+    conn.commit()
+    conn.close()
+
+# health metric tracking
+
+def add_person_steps(Step_ID, Step_Goal, Total_Steps, Daily_Steps, DL_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        INSERT INTO Steps (Step_ID, Step_Goal, Total_Steps, Daily_Steps, DL_ID)
+        VALUES (?, ?, ?, ?, ?)
+    """, (Step_ID, Step_Goal, Total_Steps, Daily_Steps, DL_ID))
+    
+    conn.commit()
+    conn.close()
+
+def retrieve_steps(DL_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+         SELECT MealLog_ID, MLSnack
+         FROM MealLog_Snack
+         WHERE MealLog_ID = ?
+    """, (DL_ID))
+    
+    steps = cursor.fetchnone()
+    conn.close()
+    return dict(steps) if steps else None
+
+def add_person_weight(Weight_ID, Total_Weight, Weight_Gain, Weight_Loss, DL_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+            INSERT INTO Weight (Weight_ID, Total_Weight, Weight_Gain, Weight_Loss, DL_ID)
+            VALUES (?, ?, ?, ?, ?)
+    """, (Weight_ID, Total_Weight, Weight_Gain, Weight_Loss, DL_ID))
+    
+    conn.commit()
+    conn.close()
+    
+def retrieve_person_weight(DL_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+           SELECT Weight_ID, Total_Weight, Weight_Gain, Weight_Loss, DL_ID
+           FROM Weight
+           WHERE DL_ID = ?
+    """, (DL_ID))
+    
+    weight = cursor.fetchnone()
+    conn.close()
+    return dict(weight) if weight else None
+
+
+def add_person_sleep(SleepHour_ID, Sleep_Quality, Hours_Slept, Sleep_Pattern, DL_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        INSERT INTO Sleep_Hours (SleepHour_ID, Sleep_Quality, Hours_Slept, Sleep_Pattern, DL_ID)
+        VALUES (?, ?, ?, ?, ?)
+    """, (SleepHour_ID, Sleep_Quality, Hours_Slept, Sleep_Pattern, DL_ID))
+    
+    conn.commit()
+    conn.close()
+    
+def retrieve_person_sleep(DL_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        SELECT SleepHour_ID, Sleep_Quality, Hours_Slept, Sleep_Pattern, DL_ID
+        FROM Sleep_Hours
+        WHERE DL_ID = ?
+    """, (DL_ID))
+    
+    sleep = cursor.fetchnone()
+    conn.close()
+    return dict(sleep) if sleep else None
+
+
+def add_person_bloodpressure(Blood_ID, Blood_Status, DL_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+            INSERT INTO Blood_Pressure (Blood_ID, Blood_Status, DL_ID)
+            VALUES (?, ?, ?)
+    """, (Blood_ID, Blood_Status, DL_ID))
+    
+    conn.commit()
+    conn.close()
+    
+def retrieve_person_bloodpressure(DL_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        SELECT Blood_ID, Blood_Status, DL_ID
+        FROM Blood_Pressure
+        WHERE DL_ID = ?
+    """, (DL_ID))
+    
+    bloodpressure = cursor.fetchnone()
+    conn.close()
+    return dict(bloodpressure) if bloodpressure else None
+
+# medication log
+
+def add_medication(Medication_ID, Medication_Time, Dosage, DL_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+       INSERT INTO Medication (Medication_ID, Medication_Time, Dosage, DL_ID)
+       VALUES (?, ?, ?, ?)
+    """, (Medication_ID, Medication_Time, Dosage, DL_ID))
+    
+    conn.commit()
+    conn.close()
+
+def retrieve_medication(DL_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        SELECT Medication_ID, Medication_Time, Dosage, DL_ID
+        FROM Medication
+        WHERE DL_ID = ?
+    """, (DL_ID))
+    
+    medication = cursor.fetchnone()
+    conn.close()
+    return dict(medication) if medication else None
+
+def update_medication(DL_ID, Medication_Time, Dosage):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+            UPDATE Medication
+            SET Medication_Time = ?,
+            SET Dosage = ?
+            WHERE DL_ID = ?
+        """, (Medication_Time, Dosage, DL_ID))
+
+    conn.commit()
+    conn.close()
+
+# medical log
+
+def add_medical_record(Medical_ID, Past_surgeries, Past_Medication, PU_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+       INSERT INTO Medical_Record (Medical_ID, Past_surgeries, Past_Medication, PU_ID)
+        VALUES (?, ?, ?, ?)
+    """, (Medical_ID, Past_surgeries, Past_Medication, PU_ID))
+    
+    conn.commit()
+    conn.close()
+
+def retrieve_medical_record(PU_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        SELECT Medical_ID, Past_surgeries, Past_Medication, PU_ID
+        FROM Medical_Record
+        WHERE PU_ID = ?
+    """, (PU_ID))
+    
+    record = cursor.fetchnone()
+    conn.close()
+    return dict(record) if record else None
+
+def update_medical_record(PU_ID, Medical_ID, Past_surgeries, Past_Medication):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        UPDATE Medical_Record
+        SET Past_surgeries = 'Tonsil removal',
+         Past_Medication = 'Tylenol'
+        WHERE PU_ID = ?
+        """, (Medical_ID, Past_surgeries, Past_Medication, PU_ID))
+
+    conn.commit()
+    conn.close()
+
+def add_medical_record_allegies(Medical_ID, MRAllegies):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+       INSERT INTO Medical_Record_Allegies (Medical_ID, MRAllegies)
+       VALUES (?, ?)
+    """, (Medical_ID, MRAllegies))
+
+    conn.commit()
+    conn.close()
+    
+def retrieve_medical_record_allegies(Medical_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        SELECT Medical_ID, MRAllegies
+        FROM Medical_Record_Allegies
+        WHERE Medical_ID = ?
+    """, (Medical_ID))
+    
+    allegies = cursor.fetchnone()
+    conn.close()
+    return dict(allegies) if allegies else None
+
+def update_medical_record_allegies(Medical_ID, MRAllegies):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        UPDATE Medical_Record_Allegies
+        MRAllegies = ?
+        WHERE Medical_ID = ?
+        """, (MRAllegies, Medical_ID))
+
+    conn.commit()
+    conn.close()
+
+
+def add_medical_disease(Medical_ID, MRDiseases):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+       INSERT INTO Medical_Record_Diseases (Medical_ID, MRDiseases)
+        VALUES (?, ?)
+    """, (Medical_ID, MRDiseases))
+    
+    conn.commit()
+    conn.close()
+
+def retrieve_medical_disease(Medical_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        SELECT Medical_ID, MRDiseases
+        FROM Medical_Record_Diseases
+        WHERE Medical_ID = ?
+    """, (Medical_ID))
+    
+    disease = cursor.fetchnone()
+    conn.close()
+    return dict(disease) if disease else None
+
+def update_medical_disease(Medical_ID, MRDiseases):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        UPDATE Medical_Record_Diseases
+        SET MRDiseases = ?
+        WHERE Medical_ID = ?
+        """, (MRDiseases, Medical_ID))
+
+    conn.commit()
+    conn.close()
+    
+def add_medical_disabilities(Medical_ID, MRDisabilities):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+          INSERT INTO Medical_Record_Disabilities (Medical_ID, MRDisabilities)
+          VALUES (?, ?)
+    """, (Medical_ID, MRDisabilities))
+    
+    conn.commit()
+    conn.close()
+    
+def retrieve_medical_disabilities(Medical_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        SELECT Medical_ID, MRDisabilities
+        FROM Medical_Record_Disabilities
+        WHERE Medical_ID = ?
+    """, (Medical_ID))
+    
+    disabilities = cursor.fetchnone()
+    conn.close()
+    return dict(disabilities) if disabilities else None
+
+def update_medical_disabilities(Medical_ID, MRDisabilities):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+        UPDATE Medical_Record_Disabilities
+        SET MRDisabilities = ?
+        WHERE Medical_ID = ?
+    """, (MRDisabilities, Medical_ID))
+
+    conn.commit()
+    conn.close()
+
+# daily logging
+
+def add_daily_log(Log_ID, Log_Date, PU_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+          INSERT INTO Daily_Log (Log_ID, Log_Date, PU_ID)
+          VALUES (?, ?, ?)
+    """, (Log_ID, Log_Date, PU_ID))
+
+    conn.commit()
+    conn.close()
+    
+def retrieve_daily_log(PU_ID):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("""
+          SELECT Log_ID, Log_Date, PU_ID
+          FROM Daily_Log
+          WHERE PU_ID = ?
+    """, (PU_ID))
